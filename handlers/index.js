608f637f17479aa1e1b5814e5865a7710bf7ee57
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-05-06 21:38:13 -0500

    format

--- handlers/index.js
+++ handlers/index.js
@@ -1,31 +1,31 @@
 async function firstMessage(client, message) {
-    const buttons = [
-        {
-          buttonText: {
-            displayText: "Catálogo e valores",
-          },
-          buttonId: "catalogos",
-        },
-        {
-          buttonText: {
-            displayText: "Ver redes sociais",
-          },
-          buttonId: "redes-sociais",
-        },
-        {
-          buttonText: {
-            displayText: "Falar com profissional (orçamento e dúvidas)",
-          },
-          buttonId: "falar-com-atendente",
-        },
-      ];
+  const buttons = [
+    {
+      buttonText: {
+        displayText: "Catálogo e valores",
+      },
+      buttonId: "catalogos",
+    },
+    {
+      buttonText: {
+        displayText: "Ver redes sociais",
+      },
+      buttonId: "redes-sociais",
+    },
+    {
+      buttonText: {
+        displayText: "Falar com profissional (orçamento e dúvidas)",
+      },
+      buttonId: "falar-com-atendente",
+    },
+  ];
 
-      await client.sendButtons(
-        message.from,
-        `Olá ${message.sender.shortName}! Seja bem vindo(a) ao atendimento automatizado da Agência Gênios! 🚀`,
-        buttons,
-        "Selecione uma opção"
-      );
-  }
+  await client.sendButtons(
+    message.from,
+    `Olá ${message.sender.shortName}! Seja bem vindo(a) ao atendimento automatizado da Agência Gênios! 🚀`,
+    buttons,
+    "Selecione uma opção"
+  );
+}
 
-  module.exports = {firstMessage};
+module.exports = { firstMessage };
