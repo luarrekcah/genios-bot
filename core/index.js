76fb4335a6648da4a82fca19c9663fcbff3acefe
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-05-08 02:01:39 -0500

    only send menu after 12hours

--- core/index.js
+++ core/index.js
@@ -1,39 +1,52 @@
+const fs = require("fs");
+
 const buttonHandlers = require("../handlers/buttons");
-const {firstMessage} = require("../handlers");
+const { firstMessage } = require("../handlers");
 // const { humanMessage } = require("./functions");
 
-const intervalo = 1000 * 60 * 10;
-const lastMessages = {};
-
 async function start(client) {
-    client.onMessage(async (message) => {
-      if (
-          message.fromMe ||
-          message.isGroupMsg ||
-          message.from !== "556892253537@c.us"
-        ) {
-          return;
-        }
+  client.onMessage(async (message) => {
+    if (
+      message.fromMe ||
+      message.isGroupMsg
+    ) {
+      return;
+    }
 
-      console.log(message);
-  
-      if (message.type === "buttons_response") {
-        const handler = buttonHandlers[message.selectedButtonId];
-        if (handler) {
-          //await humanMessage(client, message);
-          await handler(client, message);
-        }
-      } else {
-        const now = Date.now();
-        const lastMessage = lastMessages[message.from] || 0;
-        const elapsed = now - lastMessage;
-        if (elapsed >= intervalo) {
-          //await humanMessage(client, message);
-          await firstMessage(client, message);
-          lastMessages[message.from] = now;
-        }
+    //console.log(message);
+
+    if (message.type === "buttons_response") {
+      const handler = buttonHandlers[message.selectedButtonId];
+      if (handler) {
+        //await humanMessage(client, message);
+        await handler(client, message);
       }
-    });
-  }
+    } else {
+      let db = [];
+      try {
+        db = JSON.parse(fs.readFileSync(`${__dirname}/../db.json`));
+      } catch (err) {
+        console.error("Erro ao ler o arquivo do banco de dados:", err);
+      }
+      const index = db.findIndex((num) => num.number === message.from);
+      const now = new Date().getTime();
+      if (index !== -1 && now - db[index].timestamp < 12 * 60 * 60 * 1000) {
+        // 12 horas
+        console.log("Não enviando mensagem, tempo mínimo não atingido.");
+      } else {
+        if (index !== -1) {
+          db[index].timestamp = now;
+        } else {
+          db.push({
+            number: message.from,
+            timestamp: now,
+          });
+        }
+        fs.writeFileSync(`${__dirname}/../db.json`, JSON.stringify(db));
+        await firstMessage(client, message);
+      }
+    }
+  });
+}
 
-  module.exports = start;
+module.exports = start;
